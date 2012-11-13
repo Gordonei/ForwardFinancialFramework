28f74cc190371e3917fc142beb3f004d08d234f2
Gordon Inggs <gordon.inggs@gmail.com>
2012-11-13 02:07:52 +0000

    Started work on Maxeler FPGA code generation propoer. Good code reuse from Multicore Class, as well as further shifting of actual behaviour from Monte Carlo parent class to Multicore Child.

--- Platforms/MaxelerFPGA/MaxelerFPGA.py
+++ Platforms/MaxelerFPGA/MaxelerFPGA.py
@@ -9,3 +9,9 @@ class MaxelerFPGA:
   
   def __init__(self,instances=1):
     self.instances = instances
+    
+  def platform_directory(self):
+    return "Platforms/MulticoreCPU/maxeler_code"
+  
+  def root_directory(self):
+    return "../../.."
