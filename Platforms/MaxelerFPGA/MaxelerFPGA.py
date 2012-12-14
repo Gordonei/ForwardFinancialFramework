48f78354253c23d5b689c3da13045a294b40b429
Gordon Inggs <gordon.inggs@gmail.com>
2012-12-14 10:47:21 +0200

    Modifying the platform objects report the directory structure, and changed the default option for the Maxeler tools

--- Platforms/MaxelerFPGA/MaxelerFPGA.py
+++ Platforms/MaxelerFPGA/MaxelerFPGA.py
@@ -7,12 +7,15 @@ class MaxelerFPGA:
   name = "maxeler_fpga"
   instances = 1
   threads = 1
+  platform_directory = ""
+  root_directory = ""
   
-  def __init__(self,instances=1):
+  def __init__(self,instances=1,platform_directory="Platforms/MaxelerFPGA/maxeler_code/src/mc_solver_maxeler",root_directory="../../../../.."):
     self.instances = instances
+    self.platform_directory = platform_directory
     
   def platform_directory(self):
-    return "Platforms/MaxelerFPGA/maxeler_code"
+    return self.platform_directory
   
   def root_directory(self):
-    return "../../.."
+    return self.root_directory
--- Platforms/MulticoreCPU/MulticoreCPU.py
+++ Platforms/MulticoreCPU/MulticoreCPU.py
@@ -7,12 +7,15 @@ import multiprocessing,sys
 class MulticoreCPU:
   name = "multicore_cpu"
   threads = 1
+  platform_directory = ""
+  root_directory = ""
   
-  def __init__(self,threads=multiprocessing.cpu_count()):
+  def __init__(self,threads=multiprocessing.cpu_count(),platform_directory="Platforms/MulticoreCPU/multicore_c_code",root_directory="../../.."):
     self.threads = threads
+    self.platform_directory = platform_directory
     
   def platform_directory(self):
-    return "Platforms/MulticoreCPU/multicore_c_code"
+    return self.platform_directory
   
   def root_directory(self):
-    return "../../.."
+    return self.root_directory
