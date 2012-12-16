9c1ccf85ad767cd16c9f08fe5a18035772868337
Gordon Inggs <gordon.inggs@gmail.com>
2012-12-16 15:13:40 +0200

    Finishing off experimental build script

--- bin/single_option_experiments.py
+++ bin/single_option_experiments.py
@@ -3,3 +3,5 @@ import subprocess
 for i in [1,2,4,12,13]:
     run_cmd = ["python","mc_solver_maxeler_test_script.py","compile",">build_%d.log"%i]
     run_cmd.append(str(i))
+    print "Compiling Option Number: %d"%i
+    subprocess.check_output(run_cmd)
