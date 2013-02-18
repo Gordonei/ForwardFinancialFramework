5fdfae42a47edea1b4ad5437d38f4252e754f19b
Gordon Inggs <gordon.inggs@gmail.com>
2013-02-18 17:25:35 +0000

    Bug fixes for the changes made to the Maxeler code generation for path point behaviour improvement.

--- Platforms/MaxelerFPGA/maxeler_code/src/mc_solver_maxeler/asian_option_parameters.java
+++ Platforms/MaxelerFPGA/maxeler_code/src/mc_solver_maxeler/asian_option_parameters.java
@@ -2,7 +2,7 @@ package mc_solver_maxeler;
 
 import com.maxeler.maxcompiler.v1.kernelcompiler.types.base.HWVar;
 
-public class asian_option_parameters extends european_option_parameters 
+public class asian_option_parameters extends european_option_parameters {
 	protected final HWVar points;
 
 	public asian_option_parameters(MC_Solver_Maxeler_Base_Kernel k,HWVar time_period,HWVar call,HWVar strike_price,HWVar observation_points){
