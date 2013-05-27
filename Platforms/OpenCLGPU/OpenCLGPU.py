c8803fbace821cce1e08b20556d4dcbe4fe0f7f1
Gordon Inggs <gordon.inggs@gmail.com>
2013-05-27 13:00:08 +0100

    Change to OpenCL code, making it work again :) Not 100% sure that the Global work item/local work item/loops inside the kernel are quite right...

--- Platforms/OpenCLGPU/OpenCLGPU.py
+++ Platforms/OpenCLGPU/OpenCLGPU.py
@@ -28,7 +28,7 @@ class OpenCLGPU:
     try: self.device = self.platform.get_devices(self.device_type)[0] #Takes the first device available for the specified platform and type
     except: #If the preferred device type isn't available, just take the first available CPU to that platform
       self.device_type = pyopencl.device_type.CPU
-      self.device = self.platform.get_devices(pyopencl.device_type.CPU)[0] 
+      self.device = self.platform.get_devices(pyopencl.device_type.CPU)[0]
     
     self.context = pyopencl.Context(devices=[self.device])
     
