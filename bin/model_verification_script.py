33d471b3688005982d22807a11b125962d1dd3b9
Gordon Inggs <gordon.inggs@gmail.com>
2013-09-05 14:37:43 +0100

    Renaming model verification script (spelling mistake). Added result file creation.

--- bin/model_verification_script.py
+++ bin/model_verification_script.py
@@ -10,25 +10,29 @@ from ForwardFinancialFramework.Platforms.MaxelerFPGA import MaxelerFPGA_MonteCar
 from ForwardFinancialFramework.Platforms.MulticoreCPU import MulticoreCPU_MonteCarlo,MulticoreCPU
 from ForwardFinancialFramework.Platforms.OpenCLGPU import OpenCLGPU_MonteCarlo,OpenCLGPU
 
-if( __name__ == '__main__' and len(sys.argv)>3):
-  paths = int(sys.argv[1])
-  steps = int(sys.argv[2])
+if( __name__ == '__main__' and len(sys.argv)>4):
+  gui = sys.argv[1].lower()
+  paths = int(sys.argv[2])
+  steps = int(sys.argv[3])
   redudancy = 10
   
   start = int(numpy.log(paths)/numpy.log(10))
   end = start+steps+1
   
-  pickle_file_names = sys.argv[3:]
+  pickle_file_names = sys.argv[4:]
   
   for p_f_n in pickle_file_names:
     #Generating the Model data
     mc_solver = pickle.load(open("%s"%p_f_n,"rb"))
+    datafile = open("%s_model_verfication.csv"%p_f_n[:-2],"w")
     mc_solver.latency_model = mc_solver.generate_aggregate_latency_model()
     mc_solver.accuracy_model = mc_solver.generate_aggregate_accuracy_model()
     
     latency_data = map(mc_solver.latency_model,[10**i for i in range(start,end+2)])
     accuracy_data = map(mc_solver.accuracy_model,[10**i for i in range(start,end+2)])
-    plt.plot(accuracy_data,latency_data,"x--",label="model")
+    if(gui=="yes"):plt.plot(accuracy_data,latency_data,"x--",label="model")
+    for index,l_d in enumerate(latency_data): datafile.write("%f,%f,\n"%(l_d,accuracy_data[index]))
+    datafile.write("\n")
     
     #Generating the Test data
     #mc_solver.generate()
@@ -54,11 +58,20 @@ if( __name__ == '__main__' and len(sys.argv)>3):
 	  
 	actual_accuracy_data[-1].append(max(temp_accuracy)) #selecting the most inaccurate of the results
 
-      plt.scatter(actual_accuracy_data[-1],actual_latency_data[-1])
-      plt.scatter(numpy.mean(actual_accuracy_data[-1]),numpy.mean(actual_latency_data[-1]),color='r')
+      if(gui=="yes"):
+	plt.scatter(actual_accuracy_data[-1],actual_latency_data[-1])
+	plt.scatter(numpy.mean(actual_accuracy_data[-1]),numpy.mean(actual_latency_data[-1]),color='r')
+	
+      for index,a_l_d in enumerate(actual_latency_data[-1]): datafile.write("%f,%f,\n"%(a_l_d,actual_accuracy_data[-1][index]))
+      datafile.write("\n")
     
-  plt.legend(loc='best')
-  plt.show()
+  if(gui=="yes"):
+    ax = plt.gca()
+    ax.set_yscale('log')
+    plt.legend(loc='best')
+    plt.show()
+    
+  datafile.close()
   
 elif(__name__ == '__main__'):
-  print "usage: python pickle_model_verification_script.py [Number of  Test Paths] [Number of Test Steps] [Pickle File Name 1] [Pickle File Name 2] ... [Pickle File Name N]"
+  print "usage: python pickle_model_verification_script.py [GUI:Yes|No] [Number of  Test Paths] [Number of Test Steps] [Pickle File Name 1] [Pickle File Name 2] ... [Pickle File Name N]"
