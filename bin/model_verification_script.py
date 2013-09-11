20b850f2f3d239be58f3c9611b1912646b008bb3
Gordon Inggs <gordon.inggs@gmail.com>
2013-09-11 17:07:37 +0100

    More brackety fun. I must be getting tired!

--- bin/model_verification_script.py
+++ bin/model_verification_script.py
@@ -75,7 +75,7 @@ if(len(sys.argv)>4):
 	plt.scatter(mean_accuracy[-1],mean_latency[-1],color='r')
 	
       for i,a_l_d in enumerate(actual_latency_data[-1]): 
-	if(i==0): datafile.write("%d,%f,%f,%f,%f,\n"%(p,a_l_d,actual_accuracy_data[-1][i],mean_latency[-1]),mean_accuracy[-1]))
+	if(i==0): datafile.write("%d,%f,%f,%f,%f,\n"%(p,a_l_d,actual_accuracy_data[-1][i],mean_latency[-1],mean_accuracy[-1]))
 	else: datafile.write("%d,%f,%f,\n"%(p,a_l_d,actual_accuracy_data[-1][i]))
       datafile.write("\n")
     
