4b3773ceea0575f7230ac014ffbdfa6f26d5e1d2
Gordon Inggs <gordon.inggs@gmail.com>
2013-11-22 11:35:02 +0000

    Still with the Black-Scholes issue!

--- Platforms/MaxelerFPGA/maxeler_code/src/mc_solver_maxeler/black_scholes_underlying_parameters.java
+++ Platforms/MaxelerFPGA/maxeler_code/src/mc_solver_maxeler/black_scholes_underlying_parameters.java
@@ -3,7 +3,8 @@ package mc_solver_maxeler;
 import com.maxeler.maxcompiler.v2.kernelcompiler.types.base.DFEVar;
 
 public class black_scholes_underlying_parameters extends underlying_parameters {
-	protected final DFEVar volatility,seed,seed2;
+	protected final DFEVar volatility;
+	protected DFEVar seed,seed2;
 
 	public black_scholes_underlying_parameters(MC_Solver_Maxeler_Base_Kernel k,DFEVar rfir, DFEVar current_price,DFEVar volatility){
 		super(k,rfir,current_price);
