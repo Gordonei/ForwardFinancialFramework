85becbff9c549b4219cdf4ac8edd2eb5253acc23
Gordon Inggs <gordon.inggs@gmail.com>
2014-04-10 22:58:30 +0100

    changes to build script

--- Platforms/VivadoFPGA/vivadohls_code/hls_prj/F3_VivadoHLS_core/script.tcl
+++ Platforms/VivadoFPGA/vivadohls_code/hls_prj/F3_VivadoHLS_core/script.tcl
@@ -5,7 +5,7 @@
 ############################################################
 open_project hls_prj
 set_top vivado_activity_thread
-add_files srcs/vivado_core.c
+add_files srcs/vivado_core.c -cflags "-DFP_t=float"
 add_files srcs/heston_underlying.h -cflags "-DFP_t=float -DTAUS_BOXMULLER -Dpow=powr -Dsqrt=rsqrt -Dsin=sinf -Dcos=cosf -DVIVADOHLS"
 add_files srcs/heston_underlying.c -cflags "-DFP_t=float -DTAUS_BOXMULLER -Dpow=powr -Dsqrt=rsqrt -Dsin=sinf -Dcos=cosf -DVIVADOHLS"
 add_files srcs/gauss.h -cflags "-DFP_t=float -Dpow=powr -Dsqrt=rsqrt -Dsin=sinf -Dcos=cosf -DVIVADOHLS"
@@ -24,11 +24,12 @@ add_files srcs/asian_option.h -cflags "-DFP_t=float"
 add_files srcs/asian_option.c -cflags "-DFP_t=float"
 add_files srcs/underlying.c -cflags "-DFP_t=float"
 add_files srcs/option.c -cflags "-DFP_t=float"
+add_files -tb srcs/tb.c
 open_solution "F3_VivadoHLS_core"
 set_part {xc7z045ffg900-2}
 create_clock -period 10 -name default
 source "./hls_prj/F3_VivadoHLS_core/directives.tcl"
-#csim_design
+csim_design
 csynth_design
-#cosim_design -trace_level none
+cosim_design -trace_level none
 export_design -evaluate vhdl -format ip_catalog -vendor "imperial" -library "F3" -version "1.0"
