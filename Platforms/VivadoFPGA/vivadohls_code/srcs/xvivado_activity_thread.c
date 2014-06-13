1fc20bfb08e04a59ce0a5144a135ed7a9bc4ef5c
gi11 <gi11@ee-boxer1.ee.ic.ac.uk>
2014-06-13 13:35:54 +0100

    A few changes - C simulations working again, pipelining and overlapping of reduction and FPGA computation

--- Platforms/VivadoFPGA/vivadohls_code/srcs/xvivado_activity_thread.c
+++ Platforms/VivadoFPGA/vivadohls_code/srcs/xvivado_activity_thread.c
@@ -55,9 +55,8 @@ void * setup_reserved_mem() //returns a pointer in userspace to the device
 
     // get the address of the device in user space which will be an offset from the base 
     // that was mapped as memory is mapped at the start of a page 
-
-    mapped_dev_base = mapped_base_reserved_mem + (dev_base & RESERVED_MEM_MAP_MASK);
     close(memfd_reserved_mem);
+    mapped_dev_base = mapped_base_reserved_mem + (dev_base & RESERVED_MEM_MAP_MASK);
     return mapped_dev_base;
 }
 
@@ -97,79 +96,79 @@ XVivado_activity_thread setup_XVivado_activity_thread(void)
     return device;
 }
 
-void vivado_activity_thread_hw(standard_underlying_attributes *kernel_u_a_0,standard_derivative_attributes *kernel_o_a_0,rng_state_t *seed_0,unsigned int thread_result_addr)
+void vivado_activity_thread_hw(standard_underlying_attributes *kernel_u_a_0,standard_derivative_attributes *kernel_o_a_0,rng_state_t *seed_0,unsigned int thread_result_addr,XVivado_activity_thread *device_info)
 {
         //volatile unsigned long * vivado_core_vbase = vivado_core_userspace();
-        XVivado_activity_thread  device_info;
-	device_info = setup_XVivado_activity_thread();       
+        //XVivado_activity_thread  device_info;
+//device_info = setup_XVivado_activity_thread();       
 
     	//------------------------------------AXI Master Setup------------------------------------------
-        printf("Writing to AXI master config registers.\n");
-    	XVivado_activity_thread_SetThread_result_0(&device_info, thread_result_addr); //Okay the base address is now set for the thread result into the AXI master
-    	printf("Thread Result Addr = 0x%X\n", XVivado_activity_thread_GetThread_result_0(&device_info));
+        //printf("Writing to AXI master config registers.\n");
+    	XVivado_activity_thread_SetThread_result_0(device_info, thread_result_addr); //Okay the base address is now set for the thread result into the AXI master
+    	//printf("Thread Result Addr = 0x%X\n", XVivado_activity_thread_GetThread_result_0(&device_info));
 
-    	printf("\n\n");
+    	//printf("\n\n");
         //------------------------------------INPUT SECTION---------------------------------------------
         //PASSING kernel_u_a_0 to the device
-        XVivado_activity_thread_SetKernel_u_a_0_rfir(&device_info, *(int*)&kernel_u_a_0->rfir);
-        printf("rfir: %f\n", kernel_u_a_0->rfir);
-        XVivado_activity_thread_SetKernel_u_a_0_current_price(&device_info, *(int*)&kernel_u_a_0->current_price);
-        printf("current_price: %f\n", kernel_u_a_0->current_price);
-        XVivado_activity_thread_SetKernel_u_a_0_volatility(&device_info, *(int*)&kernel_u_a_0->volatility);
-        printf("volatility: %f\n", kernel_u_a_0->volatility);
-        XVivado_activity_thread_SetKernel_u_a_0_initial_volatility(&device_info, *(int*)&kernel_u_a_0->initial_volatility);
-        printf("initial_volatility: %f\n", kernel_u_a_0->initial_volatility);
-        XVivado_activity_thread_SetKernel_u_a_0_volatility_volatility(&device_info, *(int*)&kernel_u_a_0->volatility_volatility);
-        printf("volatility_volatility: %f\n", kernel_u_a_0->volatility_volatility);
-        XVivado_activity_thread_SetKernel_u_a_0_rho(&device_info, *(int*)&kernel_u_a_0->rho);
-        printf("rho: %f\n", kernel_u_a_0->rho);
-        XVivado_activity_thread_SetKernel_u_a_0_kappa(&device_info, *(int*)&kernel_u_a_0->kappa);
-        printf("kappa: %f\n", kernel_u_a_0->kappa);
-        XVivado_activity_thread_SetKernel_u_a_0_theta(&device_info, *(int*)&kernel_u_a_0->theta);
-        printf("theta: %f\n", kernel_u_a_0->theta);
-        XVivado_activity_thread_SetKernel_u_a_0_correlation_matrix_0_0(&device_info, *(int*)&kernel_u_a_0->correlation_matrix_0_0);
-        printf("correlation_matrix_0_0: %f\n", kernel_u_a_0->correlation_matrix_0_0);
-        XVivado_activity_thread_SetKernel_u_a_0_correlation_matrix_0_1(&device_info, *(int*)&kernel_u_a_0->correlation_matrix_0_1);
-        printf("correlation_matrix_0_1: %f\n", kernel_u_a_0->correlation_matrix_0_1);
-        XVivado_activity_thread_SetKernel_u_a_0_correlation_matrix_1_0(&device_info, *(int*)&kernel_u_a_0->correlation_matrix_1_0);
-        printf("correlation_matrix_1_0: %f\n", kernel_u_a_0->correlation_matrix_1_0);
-        XVivado_activity_thread_SetKernel_u_a_0_correlation_matrix_1_1(&device_info, *(int*)&kernel_u_a_0->correlation_matrix_1_1);
-        printf("correlation_matrix_1_1: %f\n", kernel_u_a_0->correlation_matrix_1_1);
+        XVivado_activity_thread_SetKernel_u_a_0_rfir(device_info, *(int*)&kernel_u_a_0->rfir);
+        //printf("rfir: %f\n", kernel_u_a_0->rfir);
+        XVivado_activity_thread_SetKernel_u_a_0_current_price(device_info, *(int*)&kernel_u_a_0->current_price);
+        //printf("current_price: %f\n", kernel_u_a_0->current_price);
+        XVivado_activity_thread_SetKernel_u_a_0_volatility(device_info, *(int*)&kernel_u_a_0->volatility);
+        //printf("volatility: %f\n", kernel_u_a_0->volatility);
+        XVivado_activity_thread_SetKernel_u_a_0_initial_volatility(device_info, *(int*)&kernel_u_a_0->initial_volatility);
+        //printf("initial_volatility: %f\n", kernel_u_a_0->initial_volatility);
+        XVivado_activity_thread_SetKernel_u_a_0_volatility_volatility(device_info, *(int*)&kernel_u_a_0->volatility_volatility);
+        //printf("volatility_volatility: %f\n", kernel_u_a_0->volatility_volatility);
+        XVivado_activity_thread_SetKernel_u_a_0_rho(device_info, *(int*)&kernel_u_a_0->rho);
+        //printf("rho: %f\n", kernel_u_a_0->rho);
+        XVivado_activity_thread_SetKernel_u_a_0_kappa(device_info, *(int*)&kernel_u_a_0->kappa);
+        //printf("kappa: %f\n", kernel_u_a_0->kappa);
+        XVivado_activity_thread_SetKernel_u_a_0_theta(device_info, *(int*)&kernel_u_a_0->theta);
+        //printf("theta: %f\n", kernel_u_a_0->theta);
+        XVivado_activity_thread_SetKernel_u_a_0_correlation_matrix_0_0(device_info, *(int*)&kernel_u_a_0->correlation_matrix_0_0);
+        //printf("correlation_matrix_0_0: %f\n", kernel_u_a_0->correlation_matrix_0_0);
+        XVivado_activity_thread_SetKernel_u_a_0_correlation_matrix_0_1(device_info, *(int*)&kernel_u_a_0->correlation_matrix_0_1);
+        //printf("correlation_matrix_0_1: %f\n", kernel_u_a_0->correlation_matrix_0_1);
+        XVivado_activity_thread_SetKernel_u_a_0_correlation_matrix_1_0(device_info, *(int*)&kernel_u_a_0->correlation_matrix_1_0);
+        //printf("correlation_matrix_1_0: %f\n", kernel_u_a_0->correlation_matrix_1_0);
+        XVivado_activity_thread_SetKernel_u_a_0_correlation_matrix_1_1(device_info, *(int*)&kernel_u_a_0->correlation_matrix_1_1);
+        //printf("correlation_matrix_1_1: %f\n", kernel_u_a_0->correlation_matrix_1_1);
 
-        printf("\n\n");
+        //printf("\n\n");
         //PASSING kernel_o_a_0 to the device
-        XVivado_activity_thread_SetKernel_o_a_0_second_barrier(&device_info, *(int*)&kernel_o_a_0->second_barrier);
-        printf("second_barrier: %f\n", kernel_o_a_0->second_barrier);
-        XVivado_activity_thread_SetKernel_o_a_0_barrier(&device_info, *(int*)&kernel_o_a_0->barrier);
-        printf("barrier: %f\n", kernel_o_a_0->barrier);
-        XVivado_activity_thread_SetKernel_o_a_0_out(&device_info, *(int*)&kernel_o_a_0->out);
-        printf("out: %f\n", kernel_o_a_0->out);
-        XVivado_activity_thread_SetKernel_o_a_0_down(&device_info, *(int*)&kernel_o_a_0->down);
-        printf("down: %f\n", kernel_o_a_0->down);
-        XVivado_activity_thread_SetKernel_o_a_0_strike_price(&device_info, *(int*)&kernel_o_a_0->strike_price);
-        printf("strike_price: %f\n", kernel_o_a_0->strike_price);
-        XVivado_activity_thread_SetKernel_o_a_0_time_period(&device_info, *(int*)&kernel_o_a_0->time_period);
-        printf("time_period: %f\n", kernel_o_a_0->time_period);
-        XVivado_activity_thread_SetKernel_o_a_0_call(&device_info, *(int*)&kernel_o_a_0->call);
-        printf("call: %f\n", kernel_o_a_0->call);
-        XVivado_activity_thread_SetKernel_o_a_0_points(&device_info, *(int*)&kernel_o_a_0->points);
-        printf("points: %f\n", kernel_o_a_0->points);
+        XVivado_activity_thread_SetKernel_o_a_0_second_barrier(device_info, *(int*)&kernel_o_a_0->second_barrier);
+        //printf("second_barrier: %f\n", kernel_o_a_0->second_barrier);
+        XVivado_activity_thread_SetKernel_o_a_0_barrier(device_info, *(int*)&kernel_o_a_0->barrier);
+        //printf("barrier: %f\n", kernel_o_a_0->barrier);
+        XVivado_activity_thread_SetKernel_o_a_0_out(device_info, *(int*)&kernel_o_a_0->out);
+        //printf("out: %f\n", kernel_o_a_0->out);
+        XVivado_activity_thread_SetKernel_o_a_0_down(device_info, *(int*)&kernel_o_a_0->down);
+        //printf("down: %f\n", kernel_o_a_0->down);
+        XVivado_activity_thread_SetKernel_o_a_0_strike_price(device_info, *(int*)&kernel_o_a_0->strike_price);
+        //printf("strike_price: %f\n", kernel_o_a_0->strike_price);
+        XVivado_activity_thread_SetKernel_o_a_0_time_period(device_info, *(int*)&kernel_o_a_0->time_period);
+        //printf("time_period: %f\n", kernel_o_a_0->time_period);
+        XVivado_activity_thread_SetKernel_o_a_0_call(device_info, *(int*)&kernel_o_a_0->call);
+        //printf("call: %f\n", kernel_o_a_0->call);
+        XVivado_activity_thread_SetKernel_o_a_0_points(device_info, *(int*)&kernel_o_a_0->points);
+        //printf("points: %f\n", kernel_o_a_0->points);
 
-        printf("\n\n");
+        //printf("\n\n");
         //PASSING seed_0 to the device
-        XVivado_activity_thread_SetSeed_0_s1(&device_info, *(int*)&seed_0->s1);
-        printf("s1: %d\n", seed_0->s1);
-        XVivado_activity_thread_SetSeed_0_s2(&device_info, *(int*)&seed_0->s2);
-        printf("s2: %d\n", seed_0->s2);
-        XVivado_activity_thread_SetSeed_0_s3(&device_info, *(int*)&seed_0->s3);
-        printf("s3: %d\n", seed_0->s3);
-        XVivado_activity_thread_SetSeed_0_offset(&device_info, *(int*)&seed_0->offset);
-        printf("offset: %d\n", seed_0->offset);
+        XVivado_activity_thread_SetSeed_0_s1(device_info, *(int*)&seed_0->s1);
+        //printf("s1: %d\n", seed_0->s1);
+        XVivado_activity_thread_SetSeed_0_s2(device_info, *(int*)&seed_0->s2);
+        //printf("s2: %d\n", seed_0->s2);
+        XVivado_activity_thread_SetSeed_0_s3(device_info, *(int*)&seed_0->s3);
+        //printf("s3: %d\n", seed_0->s3);
+        XVivado_activity_thread_SetSeed_0_offset(device_info, *(int*)&seed_0->offset);
+        //printf("offset: %d\n", seed_0->offset);
 
-        printf("\n\n");
-        XVivado_activity_thread_Start(&device_info); //Kick the device into starting
-        usleep(2000);
-        while(XVivado_activity_thread_IsDone(&device_info) != 1) { } //Waiting for the operation to complete
+        //printf("\n\n");
+        XVivado_activity_thread_Start(device_info); //Kick the device into starting
+        //usleep(2000);
+        //while(XVivado_activity_thread_IsDone(&device_info) != 1) { } //Waiting for the operation to complete
 
         //u32 temp_thread_result_0 = XVivado_activity_thread_GetThread_result_0(&device_info);
         //u32 temp_thread_result_sqrd_0 = XVivado_activity_thread_GetThread_result_sqrd_0(&device_info);
@@ -179,6 +178,9 @@ void vivado_activity_thread_hw(standard_underlying_attributes *kernel_u_a_0,stan
         //*thread_result_sqrd_0 = *(float*)&temp_thread_result_sqrd_0;
 }
 
+void xVivado_activity_thread_Wait(XVivado_activity_thread *device_info){
+	while(XVivado_activity_thread_IsDone(device_info) != 1) { }
+}
 
 
 
