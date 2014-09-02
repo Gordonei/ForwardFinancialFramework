24a1d68fd91e8ee6ac50dff381f448f170c2c55b
Gordon E Inggs <gi11@ee-snowball1.ee.ic.ac.uk>
2014-09-02 21:31:01 +0100

    improving scripts

--- bin/test_scripts/mc_solver_ks_bulk_test_script.py
+++ bin/test_scripts/mc_solver_ks_bulk_test_script.py
@@ -28,7 +28,7 @@ if( __name__ == '__main__' and len(sys.argv)>4):
   for p in range(paths,paths*(path_steps+1),paths):
     #for t in range(threads,threads+thread_steps): #Threads increase in powers of 2
     for o in options:
-      execution_output = mc_solver_ks_test.run_ks_solver(platform_name,p,"Execute",[o])[1]
+      execution_output = mc_solver_ks_test.run_ks_solver(platform_name,p,["Generate","Compile","Execute"],[o])[1]
 	
       result_string = "%s,%d,%s,%s,%s,%s,%s,\n"%(o,p,execution_output["Option %s"%o],execution_output["Option %s 95%%CI"%o],execution_output["User time"],execution_output["Kernel time"],execution_output["Total time"])
       print result_string
