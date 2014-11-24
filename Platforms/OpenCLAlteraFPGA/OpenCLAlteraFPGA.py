d475b768449b48ffe1ddadbc85d4c67dc1ea36bc
Gordon Inggs <gordon.inggs@gmail.com>
2014-11-24 09:17:29 +0000

    Created a proper abstract class for Platforms, and inheirit from it for the currently used platforms.

--- Platforms/OpenCLAlteraFPGA/OpenCLAlteraFPGA.py
+++ Platforms/OpenCLAlteraFPGA/OpenCLAlteraFPGA.py
@@ -1,25 +1,17 @@
 """
 Created on 1 April 2014
 """
+import ForwardFinancialFramework.Platforms.Platform as Platform
 import pyopencl
 
-class OpenCLAlteraFPGA:
+class OpenCLAlteraFPGA(Platform.Platform):
     name = "opencl_alterafpga"
-    platform_directory_string = ""
-    root_directory_string = ""
     threads = 0
     device_type = pyopencl.device_type.ALL
     board = ""
     
-    def __init__(self,platform_directory_string="Platforms/OpenCLAlteraFPGA/openclalterafpga_code/",root_directory_string="../../..",platform_name="Altera Corporation",board="pcie385n_d5"):
-        self.platform_directory_string = platform_directory_string
-        self.root_directory_string = root_directory_string
+    def __init__(self,platform_directory_string="Platforms/OpenCLAlteraFPGA/openclalterafpga_code/",root_directory_string=None,platform_name="Altera Corporation",board="pcie385n_d5"):
+        Platform.Platform.__init__(self,platform_directory_string,root_directory_string)
     
         self.platform_name = platform_name
         self.board = board
-        
-    def platform_directory(self):
-        return self.platform_directory_string
-  
-    def root_directory(self):
-        return self.root_directory_string
