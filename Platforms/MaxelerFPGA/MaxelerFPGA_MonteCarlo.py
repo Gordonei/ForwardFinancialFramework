4063ab7d576117e79fd5916d9d012f48526263aa
Gordon Inggs <gordon.inggs@gmail.com>
2014-11-30 18:12:09 +0000

    Adding remote execution for Maxeler

--- Platforms/MaxelerFPGA/MaxelerFPGA_MonteCarlo.py
+++ Platforms/MaxelerFPGA/MaxelerFPGA_MonteCarlo.py
@@ -666,15 +666,18 @@ class MaxelerFPGA_MonteCarlo(MulticoreCPU_MonteCarlo.MulticoreCPU_MonteCarlo):
       os.chdir("bin")
       return "Maxeler Code directory doesn't exist!"
     """
+    #Remote running
+    if(self.platform.remote): run_cmd = ["ssh",self.platform.ssh_alias]
+    else: run_cmd = []
     
-    run_cmd = ["%s/%sRun"%(self.platform.absolute_platform_directory(),self.output_file_name)]
+    run_cmd += ["%s/%sRun"%(self.platform.absolute_platform_directory(),self.output_file_name)]
     for k in sorted(self.solver_metadata): run_cmd += [str(self.solver_metadata[k])]
     
     for index,u_a in enumerate(self.underlying_attributes):
-        for a in u_a: run_cmd.append(str(self.underlying[index].__dict__[a])) #mirrors generation code to preserve order of variable loading
+        for a in u_a: run_cmd += [str(self.underlying[index].__dict__[a])] #mirrors generation code to preserve order of variable loading
     
     for index,o_a in enumerate(self.derivative_attributes): 
-        for a in o_a: run_cmd.append(str(self.derivative[index].__dict__[a]))
+        for a in o_a: run_cmd += [str(self.derivative[index].__dict__[a])]
     
     run_string = ""
     for r_c in run_cmd: run_string += " %s"%(r_c)
