d130aee2c1fdac6e40a7f69979467b9e0462eeaf
Gordon Inggs <gordon.inggs@gmail.com>
2014-11-30 15:48:42 +0000

    Fixing Maxeler root directory setting

--- Platforms/MaxelerFPGA/MaxelerFPGA.py
+++ Platforms/MaxelerFPGA/MaxelerFPGA.py
@@ -8,5 +8,5 @@ class MaxelerFPGA(Platform.Platform):
   name = "maxeler_fpga"
   threads = 1
   
-  def __init__(self,platform_directory_string="Platforms/MaxelerFPGA/maxeler_code/src/mc_solver_maxeler",root_directory_string="None",ssh_alias="",remote=False,hostname=""):
+  def __init__(self,platform_directory_string="Platforms/MaxelerFPGA/maxeler_code/src/mc_solver_maxeler",root_directory_string=None,ssh_alias="",remote=False,hostname=""):
     Platform.Platform.__init__(self,platform_directory_string,root_directory_string,ssh_alias,remote,hostname)
