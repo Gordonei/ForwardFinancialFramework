cb39db72fc07289185e0bb804590223f1f1217f6
Gordon Inggs <gordon.inggs@gmail.com>
2015-03-01 19:45:59 +0000

    Adding SSH execution option option test script

--- bin/test_scripts/mc_solver_option_test.py
+++ bin/test_scripts/mc_solver_option_test.py
@@ -1,10 +1,10 @@
 '''
 Created on 21 November 2014
 '''
-import sys,os
+import sys,os,time,numpy
 import ForwardFinancialFramework.bin.option_generator as option_generator
 
-def run_option_solver(platform_name,paths,script_option,options,debug=False,threads=0):
+def run_option_solver(platform_name,paths,script_option,options,debug=False,threads=0,ssh_alias=None):
  
   if(platform_name=="OpenCL_GPU"):
     from ForwardFinancialFramework.Platforms.OpenCLGPU import OpenCLGPU_MonteCarlo,OpenCLGPU
@@ -35,7 +35,8 @@ def run_option_solver(platform_name,paths,script_option,options,debug=False,thre
     print "incorrect platform type!"
     sys.exit()
     
-  platform = platform_class()
+  if(ssh_alias): platform = platform_class(remote=True,ssh_alias=ssh_alias)
+  else: platform = platform_class()
   mc_solver = mc_solver_class(options,paths,platform)#,instances=1)
     
   if("Generate" in script_option): mc_solver.generate(debug=debug)
@@ -46,8 +47,10 @@ def run_option_solver(platform_name,paths,script_option,options,debug=False,thre
   execution_output=[""]
   if ("Execute" in script_option): 
 	if("Maxeler_FPGA" in platform_name): mc_solver.dummy_run()
+	numpy.random.seed(int(time.time()))
 	execution_output = mc_solver.execute(debug=debug)
  
+  #print(execution_output)
   execution_output_dict = {}
   count = 1
   if("Execute" in script_option):
@@ -64,10 +67,11 @@ def run_option_solver(platform_name,paths,script_option,options,debug=False,thre
   return (compile_output,execution_output_dict)
     
 if(__name__ == '__main__' and len(sys.argv)>3):
-  platform_name = sys.argv[1]
-  paths = int(sys.argv[3])
-  script_option = sys.argv[2]
-  option_triples = sys.argv[4:]
+  ssh_alias = sys.argv[1]
+  platform_name = sys.argv[2]
+  paths = int(sys.argv[4])
+  script_option = sys.argv[3]
+  option_triples = sys.argv[5:]
 
   options_tuple = []
   for o_t in option_triples:
@@ -77,17 +81,21 @@ if(__name__ == '__main__' and len(sys.argv)>3):
   options_tuple = tuple(options_tuple)
 
   options = [option_generator.generate_option(underlying_type=o_t[0],option_type=o_t[1],seed=o_t[2]) for o_t in options_tuple]
-  compile_results,execute_results = run_option_solver(platform_name,paths,script_option,options,debug=True,threads=0)
+  compile_results,execute_results = run_option_solver(platform_name,paths,script_option,options,debug=True,threads=0,ssh_alias=ssh_alias)
   
   if(len(compile_results)>0):
     print "compiler output:"
     for r in compile_results: print r
     
   if(len(execute_results.keys())>3):
+    print(execute_results)
     for k in sorted(execute_results.keys()): 
       if("time" not in k): print "%s - %s"%(k,execute_results[k])
-      
-    print "Latency: %s uS (%s uS Setup Time + %s uS Activity Time)"%(execute_results["Total time"],execute_results["User time"],execute_results["Kernel time"])
     
+  
+    #time_string = "Latency: %s uS (%s uS Setup Time + %s uS Activity Time)"%(str(execute_results["Total time"]),str(execute_results["User time"]),str(execute_results["Kernel time"]))
+    time_string = "Latency: %.6f S"%float(execute_results["Total time"]/1e6)
+    print time_string    
+
 elif(__name__ == '__main__'):
-  print "usage: python mc_solver_test_script [CPU|OpenCL_GPU|Maxeler_FPGA|Vivado_FPGA|OpenCL_AlteraFPGA] [Generate|&Compile|&Execute] [Number of Paths] [Option Underlying Type 1] [Option Derivative Type 1] [Option Seed 1] [Option Underlying Type 2] [Option Derivative Type 2]  [Option Seed 2] [...] [Option Underlying Type N] [Option Derivative Type M]  [Option Seed N]"
+  print "usage: python mc_solver_test_script [SSH Alias] [CPU|OpenCL_GPU|Maxeler_FPGA|Vivado_FPGA|OpenCL_AlteraFPGA] [Generate|&Compile|&Execute] [Number of Paths] [Option Underlying Type 1] [Option Derivative Type 1] [Option Seed 1] [Option Underlying Type 2] [Option Derivative Type 2]  [Option Seed 2] [...] [Option Underlying Type N] [Option Derivative Type M]  [Option Seed N]"
