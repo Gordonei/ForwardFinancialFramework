a8fe35b30d28dd4ab475bf9ff1a779fb0faeeb7a
Gordon Inggs <gordon.inggs@gmail.com>
2015-09-06 18:04:25 +0200

    Adding generation behaviour for OpenCL Xilinx flow. Still working on compilation and execution

--- Platforms/OpenCLXilinxFPGA/OpenCLXilinxFPGA_MonteCarlo.py
+++ Platforms/OpenCLXilinxFPGA/OpenCLXilinxFPGA_MonteCarlo.py
@@ -45,11 +45,17 @@ class OpenCLXilinxFPGA_MonteCarlo(OpenCLAlteraFPGA_MonteCarlo.OpenCLAlteraFPGA_M
 		"""
 		output_list = []
 
+		output_list += ["#define OPENCL_GPU"]
+		output_list += ["#define native_exp exp"]
+      		if(self.random_number_generator=="mwc64x_boxmuller"): output_list.append += ["#define MWC64X_BOXMULLER"]
+      		elif(self.random_number_generator=="taus_boxmuller" or self.random_number_generator=="taus_ziggurat"): output_list += ["#define TAUS_BOXMULLER"]
+		output_list += ["#define SIMD_UNITS %d"%self.simd_width]
+
 		#Adding Xilinx OpenCl directives
 	 	#output_list.append("__attribute__((num_simd_work_items(SIMD_UNITS)))")
-		output_list.append("__attribute__((reqd_work_group_size(SIMD_UNITS,1,1)))")
+		output_list += ["__attribute__((reqd_work_group_size(SIMD_UNITS,1,1)))"]
 
-		output_list.extend(OpenCLGPU_MonteCarlo.OpenCLGPU_MonteCarlo.generate_kernel_definition(self,restrict_arrays=True))
+		output_list += OpenCLGPU_MonteCarlo.OpenCLGPU_MonteCarlo.generate_kernel_definition(self,restrict_arrays=True)
 		
 		#output_list.append("__attribute__((xcl_pipeline_workitems))")
 
@@ -72,4 +78,85 @@ class OpenCLXilinxFPGA_MonteCarlo(OpenCLAlteraFPGA_MonteCarlo.OpenCLAlteraFPGA_M
 		output_list.append("for(uint j=0;j<PATH_POINTS;++j){")
 		
 		return output_list
+
+	def generate_tcl_build_script(self,compile_options=[]):
+		"""Helper method for generating the tcl build script need by sdaccel to build the designs
+		"""
+		output_list = []
+
+		output_list.append("# Create SDAccel project") 
+		output_list.append("create_project -name %s -dir %s -force"%self.output_file_name,os.path.join(self.platform.root_directory(),self.platform.platform_directory()))
+		output_list.append("set_property platform %s [current_project]"%self.platform.board)
+
+		compile_str = "-lpthread -lrt"
+		for c in self.compile_define_flags(): compile_str= "%s %s"%(compile_str,c)
+		for c in self.compile_optimisation_flags(): compile_str= "%s %s"%(compile_str,c)
+		for c in compile_options: compile_str= "%s %s"%(compile_str,c)
+		
+		output_list.append("\n#Host Compiler Flags ")
+		output_list.append("set_property -name host_cflags -value \"%s\" -objects [current_project]"%compile_str)
+
+		output_list.append("\n#Host Source Files ")
+		#Main file
+		output_list.append("add_files \"%s/%s.c\""%(os.path.join(self.platform.root_directory(),self.platform.platform_directory()),self.output_file_name))
+		#Other sources
+		
+		#Including all of the derivative and option classes that are used
+		temp = []
+		for u in self.underlying:
+			if(not(u.name in temp)):
+				output_list.append(("add_files \"%s/%s.c\"" % (os.path.join(self.platform.root_directory(),self.platform.platform_directory()),u.name)))
+				temp.append(u.name)
+    
+			base_list = []
+			self.generate_base_class_names(u.__class__,base_list)
+
+			for b in base_list:
+				if(b not in temp):
+					output_list.append(("add_files \"%s/%s.c\"" % (os.path.join(self.platform.root_directory(),self.platform.platform_directory()),b)))
+					temp.append(b)
+  
+		#Random number generator file
+		output_list.append("add_files \"%s/gauss.c\""%os.path.join(self.platform.root_directory(),self.platform.platform_directory()))
+		
+		for d in self.derivative:
+			if(not(d.name in temp)):
+				output_list.append(("add_files \"%s/%s.c\"" % (os.path.join(self.platform.root_directory(),self.platform.platform_directory()),d.name)))
+				temp.append(d.name)
+	
+			base_list = []
+			self.generate_base_class_names(d.__class__,base_list)
+	
+			for b in base_list:
+				if(b not in temp):
+					add_files.append(("add_files \"%s/%s.c\"" % (os.path.join(self.platform.root_directory(),self.platform.platform_directory()),b)))
+					temp.append(b) 
+
+		output_list.append("\n#Kernel Definition ")
+		output_list.append("create_kernel %s -type clc"%self.output_file_name)
+		output_list.append("add_files -kernel [get_kernels %s] \"%s/%s.cl\""%(self.output_file_name,os.path.join(self.platform.root_directory(),self.platform.platform_directory()),self.output_file_name))
+
+		output_list.append("\n#Define the Binary Containers")
+		output_list.append("create_opencl_binary -device [lindex [get_device \"fpga0\"] 0] %s"%self.output_file_name)
+		output_list.append("set_property region \"OCL_REGION_0\" [get_opencl_binary %s]"%self.output_file_name)
+		for i in range(self.instances): output_list.append("create_compute_unit -opencl_binary [get_opencl_binary %s] -kernel [get_kernels %s] -name %s_%d"%(self.output_file_name,self.output_file_name,self.output_file_name,i))
+
+		output_list.append("\n#Build System")
+		output_list.append("build_system")
+
+		output_list.append("\n#Package System")
+		output_list.append("package_system")
+
+		return output_list
+
+	def generate(self,override=True,verbose=False,debug=False):
+		"""Overriding the generate method as a TCL script needs to be generated for SDAccel
+		
+		Parameters
+			override, debug - same as in OpenCLGPU_MonteCarlo class.
+		"""
+		tcl_code_list = self.generate_tcl_build_script()
+		self.generate_source(tcl_code_list,".tcl",verbose=verbose)
+
+		OpenCLGPU_MonteCarlo.OpenCLGPU_MonteCarlo.generate(self,override,verbose,debug)
 
